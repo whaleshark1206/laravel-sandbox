e7cc19a58e14f2d58e9f2f1afa335c217115a089
whaleshark1206 <whaleshark1206@gmail.com>
2021-01-16 17:09:21 +0900

    ・database接続情報を設定　laravel/ui vue にて認証機能の追加

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+  return view('welcome');
+});
 
-Route::get('/',  'TestController@testCommunication');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
